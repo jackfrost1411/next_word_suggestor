41b53704af13926c33c14091ce140d32761f6d17
Dhruvil Shah <48760936+jackfrost1411@users.noreply.github.com>
2020-05-07 19:51:57 +0530

    Add files via upload

---
+++ LSTM/doc3.py
@@ -0,0 +1,42 @@
+training_doc3 = """
+how many people live in atlanta georgia	Atlanta ( , stressed , locally ) is the capital of and the most populous city in the U.S. state of Georgia , with an estimated 2011 population of 432,427 .	1
+how many people live in atlanta georgia	Atlanta is the cultural and economic center of the Atlanta metropolitan area , home to 5,457,831 people and the ninth largest metropolitan area in the United States .	1
+how many people live in atlanta georgia	Atlanta is the county seat of Fulton County , and a small portion of the city extends eastward into DeKalb County .	0
+how many people live in atlanta georgia	Atlanta was established in 1837 at the intersection of two railroad lines , and the city rose from the ashes of the Civil War to become a national center of commerce .	0
+how many people live in atlanta georgia	In the decades following the Civil Rights Movement , during which the city earned a reputation as `` too busy to hate '' for the progressive views of its citizens and leaders , Atlanta attained international prominence .	0
+how many people live in atlanta georgia	Atlanta is the primary transportation hub of the Southeastern United States , via highway , railroad , and air , with Hartsfield–Jackson Atlanta International Airport being the world 's busiest airport since 1998 .	0
+how many people live in atlanta georgia	Atlanta is considered an `` alpha ( - ) world city , '' and , with a gross domestic product of US $ 270 billion , Atlanta’s economy ranks 15th among world cities and sixth in the nation .	0
+how many people live in atlanta georgia	Although Atlanta’s economy is considered diverse , dominant sectors include logistics , professional and business services , media operations , government administration , and higher education .	0
+how many people live in atlanta georgia	Topographically , Atlanta is marked by rolling hills and dense tree coverage .	0
+how many people live in atlanta georgia	Revitalization of Atlanta 's neighborhoods , initially spurred by the 1996 Olympics , has intensified in the 21st century , altering the city 's demographics , politics , and culture .	0
+how many people died at the pentagon in 9 11	The September 11 attacks ( also referred to as September 11 , September 11th , or 9/11 ) were a series of four coordinated terrorist attacks launched by the Islamic terrorist group al-Qaeda upon the United States in New York City and the Washington , D.C. area on September 11 , 2001 .	0
+how many people died at the pentagon in 9 11	Four passenger airliners were hijacked by 19 al-Qaeda terrorists so they could be flown into buildings in suicide attacks .	0
+how many people died at the pentagon in 9 11	Two of those planes , American Airlines Flight 11 and United Airlines Flight 175 , were crashed into the North and South towers , respectively , of the World Trade Center complex in New York City .	0
+how many people died at the pentagon in 9 11	Within two hours , both towers collapsed with debris and the resulting fires causing partial or complete collapse of all other buildings in the WTC complex , as well as major damage to ten other large surrounding structures .	0
+how many people died at the pentagon in 9 11	A third plane , American Airlines Flight 77 , was crashed into the Pentagon ( the headquarters of the United States Department of Defense ) , leading to a partial collapse in its western side .	0
+how many people died at the pentagon in 9 11	The fourth plane , United Airlines Flight 93 , was targeted at the United States Capitol in Washington , D.C. , but crashed into a field near Shanksville , Pennsylvania , after its passengers tried to overcome the hijackers .	0
+how many people died at the pentagon in 9 11	In total , almost 3,000 people died in the attacks , including the 227 civilians and 19 hijackers aboard the four planes .	0
+how many people died at the pentagon in 9 11	Suspicion quickly fell on al-Qaeda .	0
+how many people died at the pentagon in 9 11	Although the group 's leader , Osama bin Laden , initially denied any involvement , in 2004 he claimed responsibility for the attacks .	0
+how many people died at the pentagon in 9 11	Al-Qaeda and bin Laden cited U.S. support of Israel , the presence of U.S. troops in Saudi Arabia , and sanctions against Iraq as motives for the attacks .	0
+how many people died at the pentagon in 9 11	The United States responded to the attacks by launching the War on Terror and invading Afghanistan to depose the Taliban , which had harbored al-Qaeda .	0
+how many people died at the pentagon in 9 11	Many countries strengthened their anti-terrorism legislation and expanded law enforcement powers .	0
+how many people died at the pentagon in 9 11	Having evaded capture for years , bin Laden was located and killed by U.S. forces in May 2011 .	0
+how many people died at the pentagon in 9 11	The destruction of the Twin Towers and other properties caused serious damage to the economy of Lower Manhattan and had a significant effect on global markets .	0
+how many people died at the pentagon in 9 11	Cleanup of the World Trade Center site was completed in May 2002 , and the Pentagon was repaired within a year .	0
+how many people died at the pentagon in 9 11	Numerous memorials have been constructed , including the National September 11 Memorial & Museum in New York , the Pentagon Memorial , and the Flight 93 National Memorial in Pennsylvania .	0
+how many people died at the pentagon in 9 11	After a lengthy delay , the One World Trade Center is expected to be completed at Ground Zero in New York City in 2013 .	0
+how many people visit crater lake national park each year	Crater Lake National Park is a United States National Park located in southern Oregon .	0
+how many people visit crater lake national park each year	Established in 1902 , Crater Lake National Park is the fifth oldest national park in the United States and the only one in the state of Oregon .	0
+how many people visit crater lake national park each year	The park encompasses the caldera of Crater Lake , a remnant of a destroyed volcano , Mount Mazama , and the surrounding hills and forests .	0
+how many people visit crater lake national park each year	The lake is deep at its deepest point , which makes it the deepest lake in the United States , the second deepest in North America and the ninth deepest in the world .	0
+how many people visit crater lake national park each year	Crater Lake is often referred to as the seventh deepest lake in the world , but this former listing excludes the approximately depth of subglacial Lake Vostok in Antarctica , which resides under nearly of ice , and the recent report of a maximum depth for Lake O'Higgins /San Martin , located on the border of Chile and Argentina .	0
+how many people visit crater lake national park each year	However , when comparing its average depth of to the average depth of other deep lakes , Crater Lake becomes the deepest in the Western Hemisphere and the third deepest in the world .	0
+how many people visit crater lake national park each year	The impressive average depth of this volcanic lake is due to the nearly symmetrical deep caldera formed 7,700 years ago during the violent climactic eruptions and subsequent collapse of Mount Mazama and the relatively moist climate that is typical of the crest of the Cascade Range .	0
+how many people visit crater lake national park each year	The caldera rim ranges in elevation from .	0
+how many people visit crater lake national park each year	The United States Geological Survey benchmarked elevation of the lake surface itself is .	0
+how many people visit crater lake national park each year	This National Park encompasses .	0
+how many people visit crater lake national park each year	Crater Lake has no streams flowing into or out of it .	0
+how many people visit crater lake national park each year	All water that enters the lake is eventually lost from evaporation or subsurface seepage .	0
+how many people visit crater lake national park each year	The lake 's water commonly has a striking blue hue , and the lake is re-filled entirely from direct precipitation in the form of snow and rain .	0
+"""
